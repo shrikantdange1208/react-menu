603d140386525e4df8c0a6591c4fdf372685a0dd
sdange <sdange@paypal.com>
2023-03-15 20:03:34 +0530

    react-menu

---
+++ src/components/Categories.jsx
@@ -0,0 +1,21 @@
+const Categories = ({ categories, filterItems }) => {
+  console.log(categories)
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            key={index}
+            className="filter-btn"
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Categories
